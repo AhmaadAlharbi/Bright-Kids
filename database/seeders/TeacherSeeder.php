029a8023f439d9ac3258085bc9d0604d7edc5a14
AhmadAlharbi <ahmaadzaid7@gmail.com>
2024-10-13 13:22:39 +0300

    add pivot table classroom_teacher

--- database/seeders/TeacherSeeder.php
+++ database/seeders/TeacherSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Teacher;
+use App\Models\Classroom;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
@@ -14,5 +15,10 @@ class TeacherSeeder extends Seeder
     public function run(): void
     {
         $teachers = Teacher::factory()->count(10)->create();
+        Classroom::all()->each(function ($classroom) use ($teachers) {
+            $classroom->teachers()->attach(
+                $teachers->random(rand(1, 3))->pluck('id')->toArray()
+            );
+        });
     }
 }
